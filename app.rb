8c0e60fa56e97d05cb338df00c3fa727dc22f031
Steve <steve.hook@gmail.com>
2011-09-14 15:21:44 +0100

    Tried to integrate coffee-script and therubyracer to serve up .coffee files on demand - not working on Windows yet...

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'haml'
+require 'coffee-script'
+
+class CoffeeHandler < Sinatra::Base
+  set :views, 'public/coffee'
+
+  get "/coffee/*.coffee" do
+    filename = params[:splat].first
+    coffee filename.to_sym
+  end
+end
 
 class App < Sinatra::Base
+  use CoffeeHandler
+
   set :static, true
   set :public, 'public'
 
