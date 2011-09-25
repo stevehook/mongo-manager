6947e26766ef7559b3c23459bb9422636e5e731e
Steve Hook <steve.hook@gmail.com>
2011-09-25 12:34:31 +0100

    Assert the response.body in the /datababses spec

--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -10,10 +10,11 @@ describe "home" do
     end
   end
 
-  context "when viewing /databases" do
+  context "when fetching /databases" do
     before(:each) do
+      @database_names = ['accounts', 'human_resources', 'it_support']
       connection_stub = stub('fake_connection')
-      connection_stub.stub(:database_names).and_return(['accounts', 'human_resources', 'it_support'])
+      connection_stub.stub(:database_names).and_return(@database_names)
       Mongo::Connection.stub(:new).and_return(connection_stub)
     end
 
@@ -24,6 +25,26 @@ describe "home" do
 
     it "should return a list of database names" do
       get '/databases'
+      # TODO: Meaningful assertions
+      expected_body = @database_names.map { |name| { id: name, name: name } }.to_json
+      last_response.body.should == expected_body
+    end
+  end
+
+  context "when fetching /databases/accounts/collections" do
+    before(:each) do
+      connection_stub = stub('fake_connection')
+      connection_stub.stub(:database_names).and_return(['accounts', 'human_resources', 'it_support'])
+      Mongo::Connection.stub(:new).and_return(connection_stub)
+    end
+
+    it "should return the correct content-type" do
+      get '/databases/accounts/collections'
+      last_response.headers["Content-Type"].should =~ /application\/json/
+    end
+
+    it "should return a list of collection names" do
+      get '/databases/accounts/collections'
       # TODO: Meaningful assertions
       puts last_response.body
     end
