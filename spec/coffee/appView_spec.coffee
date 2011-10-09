59786d65ffe554d8218e9ff2b8a487f802daf1f9
Steve Hook <steve.hook@gmail.com>
2011-10-09 20:48:30 +0100

    Started implementing jasmine spec for AppView - work in progress

--- spec/coffee/appView_spec.coffee
+++ spec/coffee/appView_spec.coffee
@@ -4,4 +4,5 @@ describe 'AppView', ->
   afterEach ->
 
   it 'should render a list of databases', ->
+    window.appView = new AppView()
     expect(true).toBeTruthy()
--- spec/javascripts/appView_spec.js
+++ spec/javascripts/appView_spec.js
@@ -3,6 +3,7 @@
     beforeEach(function() {});
     afterEach(function() {});
     return it('should render a list of databases', function() {
+      window.appView = new AppView();
       return expect(true).toBeTruthy();
     });
   });
